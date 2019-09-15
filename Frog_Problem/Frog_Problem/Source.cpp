0dd876b17b983c2f2a4f87749366895dc18c8374
Unknown <gambit318@gmail.com>
2019-09-15 11:42:26 +0200

    Safety Commit.

---
+++ Frog_Problem/Frog_Problem/Source.cpp
@@ -0,0 +1,127 @@
+#include <random>
+#include "windows.h"
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+//int runs = 2000000000;
+int runs = 100000;
+
+const int lillypads_lower = 2;
+const int lillypads_upper = 35;
+
+const bool logEachSimSet = false;
+
+////////////////////////////////////////////////////////////////////////////////
+
+string GetFormattedRuns()
+{
+  string input = to_string(runs);
+  string buff = "";
+  int counter = 0;
+  for (int i = input.length() - 1; i >= 0; --i)
+  {
+    buff = input[i] + buff;
+    ++counter;
+    if (counter == 3)
+    {
+      buff = "," + buff;
+      counter = 0;
+    }
+  }
+  return buff;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+string formattedRuns = GetFormattedRuns();
+double* averages = new double[(lillypads_upper - lillypads_lower) + 1]{ 0 };
+
+typedef mt19937 rng_type;
+const uniform_real_distribution<> udist(0, 1);
+
+////////////////////////////////////////////////////////////////////////////////
+
+double CalcAverage(const int lillypads, int* bucketsArray)
+{
+  double sum = 0;
+  for (int i = 0; i < lillypads; ++i)
+  {
+    sum += (i + 1) * bucketsArray[i];
+  }
+
+  return sum / runs;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void SaveLog(const string filename, const int lillypads, int* bucketsArray, const double avg)
+{
+  ofstream txt(filename);
+
+  txt << "mean" << " : " << avg << "\n";
+  txt << "pads" << " : " << lillypads << "\n";
+  txt << "runs" << " : " << runs << " (" << formattedRuns << ")" << "\n";
+  txt << "\n";
+
+  for (int i = 0; i < lillypads; ++i)
+  {
+    txt << i + 1 << "\t:\t" << bucketsArray[i] << "\n";
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+double RunSim(rng_type& rng, const int lillypads)
+{
+  int* bucketsArray = new int[lillypads] {0};
+
+  for (int i = 0; i < runs; ++i)
+  {
+    int leaps = 0;
+    int position = 0;
+
+    while (position != lillypads)
+    {
+      position += static_cast<int>(udist(rng) * (lillypads - position)) + 1;
+      ++leaps;
+    }
+
+    ++bucketsArray[leaps - 1];
+  }
+
+  const string filename = to_string(lillypads) + "_" + to_string(abs(static_cast<int>(GetTickCount()))) + ".txt";
+  const double avg = CalcAverage(lillypads, bucketsArray);
+
+  if (logEachSimSet)
+  {
+    SaveLog(filename, lillypads, bucketsArray, avg);
+  }
+  
+  return avg;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void main()
+{
+  rng_type rng;
+  rng.seed(abs(static_cast<int>(GetTickCount())));
+
+  GetFormattedRuns();
+
+  for (int i = lillypads_lower; i <= lillypads_upper; ++i)
+  {
+    averages[i - lillypads_lower] = RunSim(rng, i);
+  }
+
+  const string filename = "AllAverages_" + to_string(abs(static_cast<int>(GetTickCount()))) + ".csv";
+  ofstream txt(filename);
+  for (int i = 0; i <= lillypads_upper - lillypads_lower; ++i)
+  {
+    txt << to_string(i + lillypads_lower) + "," + to_string(averages[i]) + "\n";
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
